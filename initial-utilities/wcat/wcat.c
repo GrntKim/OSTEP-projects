bb9c8bad9e18a139d7f2d146efb952146e1e9449
GrntKim <grantjun0224@gmail.com>
2025-11-24 12:43:16 +0900

    cleanup files

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -3,23 +3,23 @@
 #define BUFFER_SIZE 100
 
 int main(int argc, char** argv) {
-    if (argc < 2)
-        return 0;
+	if (argc < 2)
+		return 0;
 
-    int i;
-    FILE* fd;
-    char buffer[BUFFER_SIZE];
+	int i;
+	FILE* fd;
+	char buffer[BUFFER_SIZE];
 
-    for (i = 1; i < argc; i++) {
-        if ((fd = fopen(argv[i], "r")) == NULL) {
-            printf("wcat: cannot open file\n");
-            return 1;
-        }
+	for (i = 1; i < argc; i++) {
+		if ((fd = fopen(argv[i], "r")) == NULL) {
+			printf("wcat: cannot open file\n");
+			return 1;
+		}
 
-        while (fgets(buffer, BUFFER_SIZE, fd)) 
-            printf("%s", buffer);
-    }
+		while (fgets(buffer, BUFFER_SIZE, fd)) 
+			printf("%s", buffer);
+	}
 
-    fclose(fd);
-    return 0;
+	fclose(fd);
+	return 0;
 }
