f58277f6815b7ad2706fdf75eb7899b4ca09b75c
GrntKim <grantjun0224@gmail.com>
2025-11-24 14:08:05 +0900

    implemented fully-functional path command

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -7,15 +7,17 @@
 #include <stdbool.h>
 
 #define MAX_ARGS 10
-#define MAX_PATH 1024
+#define MAX_PATH 100
+#define MAX_PATH_LEN 1024
 
 const char error_message[30] = "An error has occurred\n";
 const char* built_in_cmds[] = { "exit", "cd", "path", NULL };
-char* paths[] = { "/usr/bin", "/bin", NULL };
 
 bool is_builtin(char* cmd);
 
 int main(int argc, char** argv) {
+	char* paths = (char*)malloc(MAX_PATH_LEN);
+	strcpy(paths, "/bin");
 	int i;
 	char* input = NULL;
 	size_t buffer_size = 0;
@@ -36,6 +38,7 @@ int main(int argc, char** argv) {
 		// ctrl + D: exit
 		if (nread == -1) { 
 			free(input);
+			free(paths);
 			exit(0);
 		}
 
@@ -53,30 +56,57 @@ int main(int argc, char** argv) {
 		args[arg_idx] = NULL;
 
 		if (is_builtin(args[0])) {
-			if (strcmp(args[0], "exit") == 0)
+			if (strcmp(args[0], "exit") == 0) {
+				free(input);
+				free(paths);
 				exit(0);
+			}
 			else if (strcmp(args[0], "cd") == 0) {
-				if (arg_idx == 1 || chdir(args[1]) != 0) 
+				if (arg_idx != 2) {
 					write(STDERR_FILENO, error_message, strlen(error_message));
+				} else {
+					if(chdir(args[1]) != 0) 
+					write(STDERR_FILENO, error_message, strlen(error_message));
+				}
 			} else if (strcmp(args[0], "path") == 0) {
-				printf("under construction\n");
+				if (paths != NULL) free(paths);
+				paths = (char*)malloc(MAX_PATH_LEN);
+				if (paths == NULL) {
+					write(STDERR_FILENO, error_message, strlen(error_message));
+					exit(1);
+				}
+				if (arg_idx == 1) {
+					continue;
+				} else {
+					paths[0] = '\0';
+					for (i = 1; i < arg_idx; i++) {
+						if (i > 1) strcat(paths, ":");
+						strcat(paths, args[i]);
+					}
+				}
 			}
 		} else if (arg_idx) {
-			i = 0;
 			found_path = false;
-			full_path = malloc(MAX_PATH);
+			full_path = malloc(MAX_PATH_LEN);
 			if (full_path == NULL) {
 				write(STDERR_FILENO, error_message, strlen(error_message));
 				exit(1);
 			}
-			while (paths[i] != NULL) {
-				snprintf(full_path, MAX_PATH, "%s/%s", paths[i++], args[0]);
+			char* paths_copy = strdup(paths);
+			if (paths_copy == NULL) {
+				write(STDERR_FILENO, error_message, strlen(error_message));
+				exit(1);
+			}
+			char* ptoken;
+			char* pcopy_ptr = paths_copy;
+			while ((ptoken = strsep(&pcopy_ptr, ":")) != NULL) {
+				snprintf(full_path, MAX_PATH_LEN, "%s/%s", ptoken, args[0]);
 				if (access(full_path, X_OK) == 0) {
-					args[0] = full_path;
 					found_path = true;
 					break;
 				}
 			}
+			free(paths_copy);
 			if (found_path) {
 				rc = fork();
 				if (rc < 0) { // fork fail
@@ -84,6 +114,7 @@ int main(int argc, char** argv) {
 					free(full_path);
 					exit(1);
 				} else if (rc == 0) { // child 
+					args[0] = full_path;
 					execv(args[0], args);
 					write(STDERR_FILENO, error_message, strlen(error_message));
 					free(full_path);
